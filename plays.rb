5e20f8a1878611f52e010cba992dc336349bee5d
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-08-06 16:03:07 -0400

    Implement Playwright::find_by_name(name)

--- plays.rb
+++ plays.rb
@@ -91,6 +91,17 @@ class Playwright
   end
 
   def self.find_by_name(name)
+    person = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+    return nil unless person.length > 0 # person is stored in an array
+
+    Playwright.new(person.first)
   end
 
   def initialize
