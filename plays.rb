de4d6370334f1195040916b0dd69fd2aa1e1393c
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-08-06 16:13:36 -0400

    Implement the rest of the Playwright class's methods

--- plays.rb
+++ plays.rb
@@ -104,15 +104,45 @@ class Playwright
     Playwright.new(person.first)
   end
 
-  def initialize
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
   end
 
-  def create
+  def insert
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.birth_year)
+    INSERT INTO
+    playwrights (name, birth_year)
+    VALUES
+    (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
   end
-
+  
   def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+    UPDATE
+    playwrights
+    SET
+    name = ?, birth_year = ?
+    WHERE
+    id = ?
+    SQL
   end
-
+  
   def get_plays
+    raise "#{self} not in database" unless self.id
+    plays = PlayDBConnection.instance.execute(<<-SQL, self.id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+    plays.map { |play| Play.new(play)}
   end
 end
